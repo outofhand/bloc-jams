570ea17989f5c3dcdd244528b3c27f172f7e85d0
outofhand <rnoroian@gmail.com>
2014-11-08 21:14:04 +0000

    Converted landing page to angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,31 @@
-require(["./landing"]);
-require(["./collection"]);
-require(["./album"]);
-require("./profile");
+//require(["./landing"]);
+//require(["./collection"]);
+//require(["./album"]);
+//require("./profile");
+
+angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+  $scope.mainText = "Bloc Jams";
+  $scope.subText = "Turn the music up";
+
+  $scope.albumURLs = [
+    '/images/album-placeholders/album-1.jpg',
+    '/images/album-placeholders/album-2.jpg',
+    '/images/album-placeholders/album-3.jpg',
+    '/images/album-placeholders/album-4.jpg',
+    '/images/album-placeholders/album-5.jpg',
+    '/images/album-placeholders/album-6.jpg',
+    '/images/album-placeholders/album-7.jpg',
+    '/images/album-placeholders/album-8.jpg',
+    '/images/album-placeholders/album-9.jpg' 
+  ];  
+   
+  $scope.subTextClicked = function() {
+    $scope.subText += '!';
+  };
+  
+  $scope.shuffle = function(o){ //v1.0
+      for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+      return o;
+  };  
+
+}]);
