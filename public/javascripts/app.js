e0c089e70c38c5df80f73f6443b272156cfb6b06
outofhand <rnoroian@gmail.com>
2014-11-08 14:11:26 +0000

    Added a profile and subscription page with jquery tabs

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -263,6 +263,7 @@ if ( document.URL.match(/\/album.html/) ) {
 require(["./landing"]);
 require(["./collection"]);
 require(["./album"]);
+require("./profile");
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
@@ -348,6 +349,28 @@ $(document).ready(function() {
 
 	$('.selling-points .point').hover(onHoverAction, onHoverOffAction);
 });
+});
+
+;require.register("scripts/profile", function(exports, require, module) {
+var tabsContainer = ".user-profile-tabs-container"
+var selectTabHandler = function(event) {
+  $tab = $(this);
+  $(tabsContainer + " li").removeClass("active");
+  $tab.parent().addClass("active");  
+  selectedTabName = $tab.attr("href");
+  console.log(selectedTabName);
+  $(".tab-pane").addClass("hidden");
+  $(selectedTabName).removeClass("hidden");
+  event.preventDefault();
+};
+
+if (document.URL.match(/\/profile.html/)) {
+  $(document).ready(function() {
+    var $tabs = $(tabsContainer + " a");
+    $tabs.click(selectTabHandler);
+    $tabs[0].click();
+  });
+}
 });
 
 ;require.register("scripts/require", function(exports, require, module) {
