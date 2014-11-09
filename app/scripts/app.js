ccdc64d366c430ef0f71467d2507607a82d2a383
outofhand <rnoroian@gmail.com>
2014-11-09 17:53:22 +0000

    Added song template

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,11 +7,18 @@ blocJams = angular.module('BlocJams', ['ui.router']);
 blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
   
-  $stateProvider.state('landing', {
-    url: '/',
-    controller: 'Landing.controller',
-    templateUrl: '/templates/landing.html'
-  });
+  $stateProvider
+    .state('landing', {
+      url: '/',
+      controller: 'Landing.controller',
+      templateUrl: '/templates/landing.html'
+    })
+    .state('song', {
+      url: '/song',    
+      templateUrl: '/templates/song.html'
+    });  
+
+  
 }]);
 
 blocJams.controller('Landing.controller', ['$scope', function($scope) {  
